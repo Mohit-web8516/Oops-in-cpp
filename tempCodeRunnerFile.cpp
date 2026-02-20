f46d4d9228c1863f13505d70af4816a66d172029
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-20 16:44:25 +0000

    updated

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,40 @@
-Student(const Student &s) {
-    //     name = s.name;
-    //     cgpa = s.cgpa;
-    // }
+#include <iostream>
+// using namespace std;
+
+// // Abstract base class
+// class Shape {
+// public:
+//     // Pure virtual function (no body here)
+//     virtual void draw() = 0;
+// };
+
+// // Derived class
+// class Circle : public Shape {
+// public:
+//     void draw() override {
+//         cout << "Drawing a Circle" << endl;
+//     }
+// };
+
+// // Derived class
+// class Rectangle : public Shape {
+// public:
+//     void draw() override {
+//         cout << "Drawing a Rectangle" << endl;
+//     }
+// };
+
+// int main() {
+//     Shape* s;   // Base class pointer
+
+//     Circle c;
+//     Rectangle r;
+
+//     s = &c;
+//     s->draw();  // Calls Circle’s version
+
+//     s = &r;
+//     s->draw();  // Calls Rectangle’s version
+
+//     return 0;
+// }
